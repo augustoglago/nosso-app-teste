7852df3503e1d36cabef137442359160a1289597
Bruno Carneiro Dantas <bcarneirodantas@gmail.com>
2025-09-08 21:04:39 -0300

    Tela de listagem

--- nosso-app/app/(tabs)/_layout.tsx
+++ nosso-app/app/(tabs)/_layout.tsx
@@ -26,18 +26,12 @@ export default function TabLayout() {
           default: {},
         }),
       }}>
+      {/* Deixando apenas a tela de filmes na navegação */}
       <Tabs.Screen
-        name="index"
+        name="filmes"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Filmes',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="film.fill" color={color} />,
         }}
       />
     </Tabs>
